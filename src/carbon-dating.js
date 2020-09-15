cf48902187301109efc32c32e1b88723de775284
Vadim Aleksandrovich <v.alexandrovich93@gmail.com>
2020-09-16 01:35:25 +0300

    feat: implement diting search

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,8 @@
-const CustomError = require("../extensions/custom-error");
-
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
-
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+const k = 0.693 / HALF_LIFE_PERIOD;
+module.exports = dateSample = (sampleActivity) => typeof(sampleActivity) == 'string' && 
+                                                  parseFloat(sampleActivity) > 0 && 
+                                                  parseFloat(sampleActivity) <= MODERN_ACTIVITY ? 
+                                                  Math.ceil(Math.log(MODERN_ACTIVITY / parseFloat(sampleActivity)) / k) : 
+                                                  false;
