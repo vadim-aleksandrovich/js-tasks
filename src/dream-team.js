08138fad89beb3d527d719fdf934426f0ca5dc49
Vadim Aleksandrovich <v.alexandrovich93@gmail.com>
2020-09-20 16:20:40 +0300

    feat: implement Dream team function

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,8 @@
-const CustomError = require("../extensions/custom-error");
-
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = createDreamTeam = (members) =>
+  !(members instanceof Array)
+    ? false
+    : members
+        .filter((item) => typeof item == "string")
+        .map((item) => item.trim()[0].toUpperCase())
+        .sort()
+        .join("");
