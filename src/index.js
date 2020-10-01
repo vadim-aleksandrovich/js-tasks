79886125b8113e5757661021b02333dcac913e78
Vadim Aleksandrovich <v.alexandrovich93@gmail.com>
2020-10-01 22:33:37 +0300

    init: implement fucntion

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
+exports.min = min = (array) =>
+    array instanceof Array && array.length > 0
+        ? array.reduce((a, b) => (a < b ? (b = a) : b))
+        : 0;
+exports.max = max = (array) =>
+    array instanceof Array && array.length > 0
+        ? array.reduce((a, b) => (a > b ? (b = a) : b))
+        : 0;
 
-exports.min = function min (array) {
-  return 0;
-}
-
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = avg = (array) =>
+    array instanceof Array && array.length > 0
+        ? array.reduce((a, b) => a + b) / array.length
+        : 0;
