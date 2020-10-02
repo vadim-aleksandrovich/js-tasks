aee3419e5d7cca9867309dbea5eccaef25c1acb3
Vadim Aleksandrovich <v.alexandrovich93@gmail.com>
2020-10-03 00:40:09 +0300

    feat: implement repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,22 @@
-const CustomError = require("../extensions/custom-error");
+module.exports = repeater = (str, options) => {
+  let add = "";
+  let string = "";
+  let addition = options.addition;
+  const separator = options.separator || "+";
+  const repeatTimes = options.repeatTimes || 1;
+  const additionSeparator = options.additionSeparator || "|";
+  const additionRepeatTimes = options.additionRepeatTimes || 1;
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+  str == null ? (str = "null") : str;
+  addition === null ? (addition = "null") : null;
+  addition == undefined ? (addition = "") : null;
+
+  for (let i = 0; i < additionRepeatTimes - 1; i++) {
+    add += addition + additionSeparator;
+  }
+
+  add += addition;
+  string = str + add + separator;
+
+  return string.repeat(repeatTimes - 1) + str + add;
 };
-  
