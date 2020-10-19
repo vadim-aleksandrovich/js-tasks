89ba7ad7c9f13b011c47eba037d4bd07440f2282
Vadim Aleksandrovich <v.alexandrovich93@gmail.com>
2020-10-19 23:52:25 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+module.exports = check = (str, bracketsConfig) => {
+    let newArray = bracketsConfig.map((item) => item.join(""));
+    for (let i = 0; i < newArray.length; i++) {
+        if (str.includes(newArray[i])) {
+            str = str.replace(newArray[i], "");
+            i = -1;
+        }
+    }
+    return str.length === 0;
+};
